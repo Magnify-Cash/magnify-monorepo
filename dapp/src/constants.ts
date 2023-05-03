767bca003456ef89725e6d9941ca00577d8a5598
Sumit Ghosh <sumit@sumit-ghosh.com>
2023-05-03 22:03:28 +0530

    Scripts for building dapp in production

--- dapp/src/constants.ts
+++ dapp/src/constants.ts
@@ -3,7 +3,7 @@ export const NFTY_FINANCE_DEFAULT_CHAIN: number = 80001;
 export const NFTY_FINANCE_GOERLI_GRAPH: string =
   "https://api.studio.thegraph.com/query/44181/nftyfinance/0.0.9";
 export const NFTY_FINANCE_MUMBAI_GRAPH: string =
-  "https://api.studio.thegraph.com/query/44194/nftyfinance-mumbai/0.0.6";
+  "https://api.studio.thegraph.com/query/44194/nftyfinance-mumbai/0.0.7";
 export const NFTY_FINANCE_LOCAL_GRAPH: string =
   "http://localhost:8000/subgraphs/name/nftyfinance-local";
 export const NFTY_FINANCE_DEFAULT_GRAPH: string = NFTY_FINANCE_LOCAL_GRAPH;
