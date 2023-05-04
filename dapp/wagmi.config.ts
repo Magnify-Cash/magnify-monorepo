45c40a10aa420ca7f1250dcfbed9f33cc95f8bea
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-05-04 15:31:42 -0400

    @wagmi/cli back to dapp

--- dapp/wagmi.config.ts
+++ dapp/wagmi.config.ts
@@ -1,28 +1,27 @@
 import { defineConfig } from "@wagmi/cli";
 import { hardhat } from "@wagmi/cli/plugins";
 import { react } from "@wagmi/cli/plugins";
-import * as dotenv from "dotenv";
-dotenv.config();
+import * as deployments from "../deployments.json";
 
 export default defineConfig({
   out: "src/wagmi/generated.ts",
   plugins: [
-    hardhat({
-      commands: {
-        clean: "yarn hardhat clean",
-        build: "yarn hardhat compile",
-        rebuild: "yarn hardhat compile",
-      },
-      project: "../nftyfinance-contracts-v2",
-      deployments: {
-        NFTYLending: {
-          5: "0xF4F1C3db0205d055f5A081482e0A84Ff71c42102", // Goerli
-          // @ts-ignore
-          31337: process.env.LOCAL_NFTYLENDING_ADDRESS, // Hardhat
-          80001: "0x348db0CDC8031901d126878bF0DF663FbB5afc1e", // Mumbai
-        },
-      },
-    }),
-    react(),
+  hardhat({
+    commands: {
+      clean: "yarn hardhat clean",
+      build: "yarn hardhat compile",
+      rebuild: "yarn hardhat compile",
+    },
+    project: "../contracts",
+    deployments: {
+    NFTYLending: {
+      5: "0xF4F1C3db0205d055f5A081482e0A84Ff71c42102", // Goerli
+      // @ts-ignore
+      31337: deployments.nftyLending.address, // Hardhat
+      80001: "0x348db0CDC8031901d126878bF0DF663FbB5afc1e", // Mumbai
+    },
+    },
+  }),
+  react(),
   ],
 });
