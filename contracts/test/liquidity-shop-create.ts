4e7239d2ab3f45e8de05ea85bc1390d2dbaa0183
Sumit Ghosh <sumit@sumit-ghosh.com>
2023-05-17 19:32:55 +0530

    Add tests for ERC1155 support in createLiquidityShop

--- contracts/test/liquidity-shop-create.ts
+++ contracts/test/liquidity-shop-create.ts
@@ -37,7 +37,7 @@ describe("Create liquidity shop", function () {
     ).to.be.revertedWith("invalid erc20");
   });
 
-  it("should fail for zero addr ERC721", async () => {
+  it("should fail for zero addr NFT collection", async () => {
     const { nftyLending, erc20 } = await loadFixture(
       deployNftyLendingWithTestTokens
     );
@@ -48,6 +48,50 @@ describe("Create liquidity shop", function () {
         erc20.address,
         ethers.constants.AddressZero, // zero address
         false,
+        liquidityAmount,
+        interestA,
+        interestB,
+        interestC,
+        maxOffer,
+        automaticApproval,
+        allowRefinancingTerms
+      )
+    ).to.be.revertedWith("invalid nft collection");
+  });
+
+  it("should fail for invalid ERC721", async () => {
+    const { nftyLending, erc20, erc1155 } = await loadFixture(
+      deployNftyLendingWithTestTokens
+    );
+
+    await expect(
+      nftyLending.createLiquidityShop(
+        name,
+        erc20.address,
+        erc1155.address,
+        false, // should have been true
+        liquidityAmount,
+        interestA,
+        interestB,
+        interestC,
+        maxOffer,
+        automaticApproval,
+        allowRefinancingTerms
+      )
+    ).to.be.revertedWith("invalid nft collection");
+  });
+
+  it("should fail for invalid ERC1155", async () => {
+    const { nftyLending, erc20, erc721 } = await loadFixture(
+      deployNftyLendingWithTestTokens
+    );
+
+    await expect(
+      nftyLending.createLiquidityShop(
+        name,
+        erc20.address,
+        erc721.address,
+        true, // should have been false
         liquidityAmount,
         interestA,
         interestB,
@@ -242,6 +286,80 @@ describe("Create liquidity shop", function () {
     expect(liquidityShop.erc20).to.equal(erc20.address);
     expect(liquidityShop.nftCollection).to.equal(erc721.address);
     expect(liquidityShop.nftCollectionIsErc1155).to.equal(false);
+    expect(liquidityShop.owner).to.equal(lender.address);
+    expect(liquidityShop.balance).to.equal(liquidityAmount);
+    expect(liquidityShop.interestA).to.equal(interestA);
+    expect(liquidityShop.interestB).to.equal(interestB);
+    expect(liquidityShop.interestC).to.equal(interestC);
+    expect(liquidityShop.maxOffer).to.equal(maxOffer);
+    expect(liquidityShop.name).to.equal(name);
+    expect(liquidityShop.status).to.equal(SHOP_STATUS.ACTIVE);
+    expect(liquidityShop.automaticApproval).to.equal(automaticApproval);
+    expect(liquidityShop.allowRefinancingTerms).to.equal(allowRefinancingTerms);
+  });
+
+  it("should create liquidity shop for ERC1155", async () => {
+    const { nftyLending, erc20, erc1155 } = await loadFixture(
+      deployNftyLendingWithTestTokens
+    );
+    const [owner, lender] = await ethers.getSigners();
+
+    // Get ERC20 and approve
+    await erc20.connect(lender).mint(liquidityAmount);
+    await erc20.connect(lender).approve(nftyLending.address, liquidityAmount);
+
+    // Create liquidity shop
+    const tx = await nftyLending
+      .connect(lender)
+      .createLiquidityShop(
+        name,
+        erc20.address,
+        erc1155.address,
+        true,
+        liquidityAmount,
+        interestA,
+        interestB,
+        interestC,
+        maxOffer,
+        automaticApproval,
+        allowRefinancingTerms
+      );
+
+    // Check emitted event
+    await expect(tx)
+      .emit(nftyLending, "LiquidityShopCreated")
+      .withArgs(
+        lender.address,
+        erc20.address,
+        erc1155.address,
+        interestA,
+        interestB,
+        interestC,
+        maxOffer,
+        liquidityAmount,
+        anyValue,
+        name,
+        automaticApproval,
+        allowRefinancingTerms
+      );
+
+    // Check ERC20 balances
+    expect(await erc20.balanceOf(lender.address)).to.equal(0);
+    expect(await erc20.balanceOf(nftyLending.address)).to.equal(
+      liquidityAmount
+    );
+
+    // Get liquidity shop from storage
+    const { events } = await tx.wait();
+    const event = events?.find(
+      (event) => event.event == "LiquidityShopCreated"
+    )?.args;
+    const liquidityShop = await nftyLending.liquidityShops(event?.id);
+
+    // Asserts
+    expect(liquidityShop.erc20).to.equal(erc20.address);
+    expect(liquidityShop.nftCollection).to.equal(erc1155.address);
+    expect(liquidityShop.nftCollectionIsErc1155).to.equal(true);
     expect(liquidityShop.owner).to.equal(lender.address);
     expect(liquidityShop.balance).to.equal(liquidityAmount);
     expect(liquidityShop.interestA).to.equal(interestA);
