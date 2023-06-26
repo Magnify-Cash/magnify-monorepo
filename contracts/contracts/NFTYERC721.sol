c037c878de90cc35ad48ce7fe3771637a215884b
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-06-25 21:09:32 -0400

    minor 721 cleanup

--- contracts/contracts/NFTYERC721.sol
+++ contracts/contracts/NFTYERC721.sol
@@ -14,24 +14,35 @@ contract NFTYERC721 is INFTYERC721, ERC721, AccessControl, ReentrancyGuard {
     using Address for address;
     using Strings for uint256;
 
+    /* *********** */
+    /*   STORAGE   */
+    /* *********** */
     string public baseURI;
-
-    // Access control
     bytes32 public constant NFTY_LENDING_ROLE = keccak256("NFTY_LENDING_ROLE");
-    bytes32 public constant BASE_URI_SETTER_ROLE =
-        keccak256("BASE_URI_SETTER_ROLE");
+    bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
 
+    /* *********** */
+    /*   EVENTS    */
+    /* *********** */
+    event BaseURISet(address indexed caller, string indexed baseURI);
+
+    /* *********** */
+    /* CONSTRUCTOR */
+    /* *********** */
     constructor(
         string memory name,
         string memory symbol,
         string memory _baseURI
     ) ERC721(name, symbol) {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(BASE_URI_SETTER_ROLE, msg.sender);
+        _setupRole(URI_SETTER_ROLE, msg.sender);
 
         baseURI = _baseURI;
     }
 
+    /* *********** */
+    /* FUNCTIONS   */
+    /* *********** */
     function mint(
         address to,
         uint256 tokenId
@@ -52,18 +63,15 @@ contract NFTYERC721 is INFTYERC721, ERC721, AccessControl, ReentrancyGuard {
         return baseURI;
     }
 
-    event BaseURISet(address indexed caller, string indexed baseURI);
-
     function setBaseURI(
         string memory _baseURI
-    ) external nonReentrant onlyRole(BASE_URI_SETTER_ROLE) {
+    ) external nonReentrant onlyRole(URI_SETTER_ROLE) {
         require(bytes(_baseURI).length > 0, "base URI cannot be empty");
         baseURI = _baseURI;
         emit BaseURISet(msg.sender, _baseURI);
     }
 
     // The following functions are overrides required by Solidity.
-
     function supportsInterface(
         bytes4 interfaceId
     )
