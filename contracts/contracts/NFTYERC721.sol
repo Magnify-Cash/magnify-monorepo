9e2965e1e2b8b9af3bd1cbb3ec4074efab369841
Sumit Ghosh <sumit@sumit-ghosh.com>
2023-06-28 19:37:04 +0530

    Refactor roles in NFTYERC721

--- contracts/contracts/NFTYERC721.sol
+++ contracts/contracts/NFTYERC721.sol
@@ -13,8 +13,7 @@ contract NFTYERC721 is INFTYERC721, ERC721, AccessControl, ReentrancyGuard {
     /*   STORAGE   */
     /* *********** */
     string public baseURI;
-    bytes32 public constant NFTY_LENDING_ROLE = keccak256("NFTY_LENDING_ROLE");
-    bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
+    bytes32 public constant NFTY_FINANCE_ROLE = keccak256("NFTY_FINANCE_ROLE");
 
     /* *********** */
     /*   EVENTS    */
@@ -30,7 +29,6 @@ contract NFTYERC721 is INFTYERC721, ERC721, AccessControl, ReentrancyGuard {
         string memory _baseURI
     ) ERC721(name, symbol) {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(URI_SETTER_ROLE, msg.sender);
 
         baseURI = _baseURI;
     }
@@ -39,33 +37,33 @@ contract NFTYERC721 is INFTYERC721, ERC721, AccessControl, ReentrancyGuard {
     /* FUNCTIONS   */
     /* *********** */
     /**
-     * @dev Call _safeMint but require NFTY_LENDING_ROLE
+     * @dev Call _safeMint but require NFTY_FINANCE_ROLE
      */
     function mint(
         address to,
         uint256 tokenId
-    ) external nonReentrant onlyRole(NFTY_LENDING_ROLE) {
+    ) external nonReentrant onlyRole(NFTY_FINANCE_ROLE) {
         require(to != address(0), "to address cannot be zero");
         require(!_exists(tokenId), "token already exists");
         _safeMint(to, tokenId);
     }
 
     /**
-     * @dev Call _safeMint but require NFTY_LENDING_ROLE
+     * @dev Call _safeMint but require NFTY_FINANCE_ROLE
      */
     function burn(
         uint256 tokenId
-    ) external nonReentrant onlyRole(NFTY_LENDING_ROLE) {
+    ) external nonReentrant onlyRole(NFTY_FINANCE_ROLE) {
         require(_exists(tokenId), "token does not exist");
         _burn(tokenId);
     }
 
     /**
-     * @dev Update base URI but require URI_SETTER_ROLE
+     * @dev Update base URI but require DEFAULT_ADMIN_ROLE
      */
     function setBaseURI(
         string memory _baseURI
-    ) external nonReentrant onlyRole(URI_SETTER_ROLE) {
+    ) external nonReentrant onlyRole(DEFAULT_ADMIN_ROLE) {
         require(bytes(_baseURI).length > 0, "base URI cannot be empty");
         baseURI = _baseURI;
         emit BaseURISet(msg.sender, _baseURI);
