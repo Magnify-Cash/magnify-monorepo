69674671d7aabb2bd1e7b59afe98bffdc8354e58
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-08-24 12:20:13 -0400

    loan card

--- dapp-v1/src/components/LoanCard.tsx
+++ dapp-v1/src/components/LoanCard.tsx
@@ -4,35 +4,55 @@ interface ILoanCardProps {
 	type: string;
 }
 
+
+
 export const LoanCard = (props:ILoanCardProps) => {
 	return (
 		<div className="col-sm-6 col-xl-4">
+			<style>
+				{`
+				  .progress {margin-bottom:0;}
+				  .start {float:left;}
+				  .end {float:right; text-align:right;}
+				`}
+			  </style>
+
 			<div className="card border-0 shadow rounded-4 h-100">
 				<div className="card-body">
 					<div className="specific-w-100 specific-h-100 mx-auto d-flex align-items-center justify-content-center bg-primary-subtle text-primary-emphasis rounded-circle overflow-hidden">
-						<img src="/images/placeholder/doodles.png" alt="test" height="100%" width="100%"/>
+						<img className="object-fit-cover" src="/images/placeholder/doodles.png" alt="test" height="100%"/>
 					</div>
 					<div className="text-center mt-3">
 						<h5>[Collection Name] #[NFT ID]</h5>
 						<div className="row g-4">
 							<div className="col-6 bg-info">
-							<p>Amount Borrowed</p>
-							<p>[x] [Currency]</p>
+								<i className="fa-regular fa-hand-holding-dollar h1 me-1"></i>
+								<p>[x] [Currency]</p>
+								<small>borrowed</small>
 							</div>
 							<div className="col-6 bg-success">
-							<p>Payoff Amount</p>
-							<p>[x] [Currency]</p>
+								<i className="fa-regular fa-calendar h1 me-1"></i>
+								<p>[x] [Currency]</p>
+								<small>payoff amount</small>
 							</div>
 							<div className="col-12">
-								[x] Days Left
+								<h5 className="text-start">[x] Days Left</h5>
 								<div className="progress my-2">
 								  <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow={75} aria-valuemin={0} aria-valuemax={100} style={{width: "75%"}}/>
+								</div>
+								<div className="start">
+									<p className="text-start m-0">[x date]</p>
+									<small>loan issued</small>
+								</div>
+							    <div className="end">
+									<p className="text-end m-0">[x date]</p>
+									<small>due date</small>
 								</div>
 							</div>
 						</div>
 						{props.type =="borrower" &&
 						<PopupTransaction
-						btnClass="btn btn-primary btn-lg mt-2 mb-4 ms-auto"
+						btnClass="btn btn-primary btn-lg mt-5"
 						btnText="Finalize Lending Desk"
 						modalId="txModal"
 						modalBtnText="Launch Lending Desk"
