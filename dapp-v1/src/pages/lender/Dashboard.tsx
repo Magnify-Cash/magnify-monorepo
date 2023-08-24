7b9da1065500de87d6739fa6ee347b0bc2946b66
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-08-24 13:42:54 -0400

    lender dashboard ux

--- dapp-v1/src/pages/lender/Dashboard.tsx
+++ dapp-v1/src/pages/lender/Dashboard.tsx
@@ -1,5 +1,62 @@
 import { LoanCard, PopupTransaction } from "@/components";
 
+const LoanCardParent = () => (
+	<div><h5>Lending Desk [x]</h5>
+	<div className="col-12">
+	<div className="card border-0 shadow rounded-4 h-100">
+	<div className="card-body">
+		<div className="d-lg-flex">
+			<div className="col-sm-6 col-xl-3">
+				<div className="d-flex align-items-center">
+					<div className="specific-w-50 specific-h-50 d-flex align-items-center justify-content-center bg-primary-subtle text-primary-emphasis rounded-circle flex-shrink-0">
+						<i className="fa-solid fa-hexagon-vertical-nft h2 m-0"></i>
+					</div>
+					<div className="ps-3">
+						<h3 className="m-0">[x]</h3>
+						<p className="m-0 text-primary-emphasis">loans</p>
+					</div>
+				</div>
+			</div>
+			<div className="col-sm-6 col-xl-3">
+				<div className="d-flex align-items-center">
+					<div className="specific-w-50 specific-h-50 d-flex align-items-center justify-content-center bg-primary-subtle text-primary-emphasis rounded-circle flex-shrink-0">
+						<i className="fa-solid fa-square-dollar h2 m-0"></i>
+					</div>
+					<div className="ps-3">
+						<h3 className="m-0">[currency]</h3>
+						<p className="m-0 text-primary-emphasis">currency</p>
+					</div>
+				</div>
+			</div>
+			<div className="col-sm-6 col-xl-3">
+				<div className="d-flex align-items-center">
+					<div className="specific-w-50 specific-h-50 d-flex align-items-center justify-content-center bg-primary-subtle text-primary-emphasis rounded-circle flex-shrink-0">
+						<i className="fa-solid fa-sack-dollar h2 m-0"></i>
+					</div>
+					<div className="ps-3">
+						<h3 className="m-0">[amount] <small className="fw-normal">[currency]</small></h3>
+						<p className="m-0 text-primary-emphasis">borrowed</p>
+					</div>
+				</div>
+			</div>
+			<div className="col-sm-6 col-xl-3">
+				<div className="d-flex align-items-center">
+					<div className="specific-w-50 specific-h-50 d-flex align-items-center justify-content-center bg-primary-subtle text-primary-emphasis rounded-circle flex-shrink-0">
+						<i className="fa-solid fa-sack-dollar h2 m-0"></i>
+					</div>
+					<div className="ps-3">
+						<h3 className="m-0">[amount] <small className="fw-normal">[currency]</small></h3>
+						<p className="m-0 text-primary-emphasis">balance</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+	</div>
+	</div>
+)
+
 export const Dashboard = (props:any) => {
 	// modal submit
 	function handleModalSubmit(loanID:number){
@@ -29,6 +86,7 @@ export const Dashboard = (props:any) => {
 				{/* Active Row */}
 				<div className="tab-pane fade show active" id="pills-active" role="tabpanel" aria-labelledby="pills-active-tab">
 				<div className="row g-4 g-xl-5">
+					<LoanCardParent/>
 					<LoanCard loanInfo={null}/>
 				</div>
 				</div>
@@ -37,30 +95,31 @@ export const Dashboard = (props:any) => {
 				{/* Pending Default Row */}
 				<div className="tab-pane fade" id="pills-pending-default" role="tabpanel" aria-labelledby="pills-pending-default-tab">
 				<div className="row g-4 g-xl-5">
-					  <LoanCard
-					  popupTx={
-						  <PopupTransaction
-						  btnClass="btn btn-primary btn-lg mt-4"
-						  btnText="Liquidate Loan"
-						  modalId="txModal"
-						  modalBtnText="Liquidate Now"
-						  modalFunc={() => handleModalSubmit(1)}
-						  modalTitle="Liquidate Loan"
-						  modalContent={
-							  <div>
-								  <small>Loan Details</small>
-								  <h6>Collection Name] #[NFT ID]</h6>
-								  <div className="row g-4">
-									  <div className="col-6 bg-secondary">test</div>
-									  <div className="col-6 bg-secondary">test</div>
-									  <div className="col-6 bg-secondary">test</div>
-									  <div className="col-6 bg-secondary">test</div>
-									  <div className="col-12 bg-success">test</div>
-								  </div>
-								  <hr/>
-							  </div>
-							  }
-						  />
+					<LoanCardParent/>
+				  	<LoanCard
+				  	popupTx={
+					  	<PopupTransaction
+					  	btnClass="btn btn-primary btn-lg mt-4"
+					  	btnText="Liquidate Loan"
+					  	modalId="txModal"
+					  	modalBtnText="Liquidate Now"
+					  	modalFunc={() => handleModalSubmit(1)}
+					  	modalTitle="Liquidate Loan"
+					  	modalContent={
+						  	<div>
+							  	<small>Loan Details</small>
+							  	<h6>Collection Name] #[NFT ID]</h6>
+							  	<div className="row g-4">
+								  	<div className="col-6 bg-secondary">test</div>
+								  	<div className="col-6 bg-secondary">test</div>
+								  	<div className="col-6 bg-secondary">test</div>
+								  	<div className="col-6 bg-secondary">test</div>
+								  	<div className="col-12 bg-success">test</div>
+							  	</div>
+							  	<hr/>
+						  	</div>
+						  	}
+					  	/>
 					  }
 					  loanInfo={null}/>
 				  </div>
@@ -70,7 +129,8 @@ export const Dashboard = (props:any) => {
 				{/* Defaulted Row */}
 				<div className="tab-pane fade" id="pills-defaulted" role="tabpanel" aria-labelledby="pills-defaulted-tab">
 				<div className="row g-4 g-xl-5">
-					  <LoanCard loanInfo={null}/>
+						<LoanCardParent/>
+					    <LoanCard loanInfo={null}/>
 				  </div>
 				</div>
 				{/* End Defaulted Row */}
@@ -78,7 +138,8 @@ export const Dashboard = (props:any) => {
 				{/* Completed Row */}
 				<div className="tab-pane fade" id="pills-completed" role="tabpanel" aria-labelledby="pills-completed-tab">
 				  <div className="row g-4 g-xl-5">
-					  <LoanCard loanInfo={null}/>
+					  	<LoanCardParent/>
+					  	<LoanCard loanInfo={null}/>
 				  </div>
 				</div>
 				{/* End Completed Row */}
