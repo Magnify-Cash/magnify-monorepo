4dc49d0e44f94d733c31c0d6364fb1090116f15c
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-08-28 13:09:58 -0400

    wagmi name

--- dapp-v1/src/wagmi.ts
+++ dapp-v1/src/wagmi.ts
@@ -7,7 +7,7 @@ const walletConnectProjectId = "6f26f99d86d880b561988f69808456d3";
 export const config = createConfig(
   getDefaultConfig({
     autoConnect: true,
-    appName: "My wagmi + ConnectKit App",
+    appName: "NFTY.Finance",
     walletConnectProjectId,
     chains: [mainnet, polygon, hardhat],
   })
