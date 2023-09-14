b84472ca03b139072f84330e231428eb6323fa6f
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-09-14 17:55:58 -0400

    lender dashboard READ

--- dapp-v1/src/pages/lender/Dashboard.tsx
+++ dapp-v1/src/pages/lender/Dashboard.tsx
@@ -1,72 +1,17 @@
-import { LoanRow, PopupTransaction } from "@/components";
-
-const LoanCardParent = () => (
-  <div>
-    <h5>Lending Desk [x]</h5>
-    <div className="col-12">
-      <div className="card border-0 shadow rounded-4 h-100">
-        <div className="card-body">
-          <div className="d-lg-flex">
-            <div className="col-sm-6 col-xl-3">
-              <div className="d-flex align-items-center">
-                <div className="specific-w-50 specific-h-50 d-flex align-items-center justify-content-center bg-primary-subtle text-primary-emphasis rounded-circle flex-shrink-0">
-                  <i className="fa-solid fa-hexagon-vertical-nft h2 m-0"></i>
-                </div>
-                <div className="ps-3">
-                  <h3 className="m-0">[x]</h3>
-                  <p className="m-0 text-primary-emphasis">loans</p>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-6 col-xl-3">
-              <div className="d-flex align-items-center">
-                <div className="specific-w-50 specific-h-50 d-flex align-items-center justify-content-center bg-primary-subtle text-primary-emphasis rounded-circle flex-shrink-0">
-                  <i className="fa-solid fa-square-dollar h2 m-0"></i>
-                </div>
-                <div className="ps-3">
-                  <h3 className="m-0">[currency]</h3>
-                  <p className="m-0 text-primary-emphasis">currency</p>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-6 col-xl-3">
-              <div className="d-flex align-items-center">
-                <div className="specific-w-50 specific-h-50 d-flex align-items-center justify-content-center bg-primary-subtle text-primary-emphasis rounded-circle flex-shrink-0">
-                  <i className="fa-solid fa-sack-dollar h2 m-0"></i>
-                </div>
-                <div className="ps-3">
-                  <h3 className="m-0">
-                    [amount] <small className="fw-normal">[currency]</small>
-                  </h3>
-                  <p className="m-0 text-primary-emphasis">borrowed</p>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-6 col-xl-3">
-              <div className="d-flex align-items-center">
-                <div className="specific-w-50 specific-h-50 d-flex align-items-center justify-content-center bg-primary-subtle text-primary-emphasis rounded-circle flex-shrink-0">
-                  <i className="fa-solid fa-sack-dollar h2 m-0"></i>
-                </div>
-                <div className="ps-3">
-                  <h3 className="m-0">
-                    [amount] <small className="fw-normal">[currency]</small>
-                  </h3>
-                  <p className="m-0 text-primary-emphasis">balance</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-);
+import { useQuery } from "urql";
+import { useAccount } from "wagmi";
+import { LoanRow } from "@/components";
+import { LenderDashboardDocument } from "../../../.graphclient";
 
 export const Dashboard = (props: any) => {
-  // modal submit
-  function handleModalSubmit(loanID: number) {
-    console.log("loanID", loanID);
-  }
+  // GraphQL
+  const { address } = useAccount();
+  const [result] = useQuery({
+    query: LenderDashboardDocument,
+    variables: {
+      walletAddress: address,
+    },
+  });
 
   return (
     <div className="container-md px-3 px-sm-4 px-xl-5">
@@ -140,8 +85,16 @@ export const Dashboard = (props: any) => {
           aria-labelledby="pills-active-tab"
         >
           <div className="row g-4 g-xl-5">
-            <LoanCardParent />
-            <LoanRow loans={[]} />
+            {
+              result?.data?.lendingDesks
+                .map((desk) =>{
+                  return (
+                  <LoanCardParent desk={desk} status="Active">
+                    <LoanRow loans={desk.loans} status="Active"/>
+                  </LoanCardParent>
+                )
+                })
+            }
           </div>
         </div>
         {/* End Active Row */}
@@ -154,9 +107,16 @@ export const Dashboard = (props: any) => {
           aria-labelledby="pills-pending-default-tab"
         >
           <div className="row g-4 g-xl-5">
-            <LoanCardParent />
-            <LoanRow loans={[]}
-            />
+            {
+              result?.data?.lendingDesks
+                .map((desk) =>{
+                  return (
+                  <LoanCardParent desk={desk} status="Defaulted">
+                    <LoanRow loans={desk.loans} status="Defaulted"/>
+                  </LoanCardParent>
+                )
+                })
+            }
           </div>
         </div>
         {/* End Pending Default Row */}
@@ -169,8 +129,16 @@ export const Dashboard = (props: any) => {
           aria-labelledby="pills-defaulted-tab"
         >
           <div className="row g-4 g-xl-5">
-            <LoanCardParent />
-            <LoanRow loans={[]} />
+            {
+              result?.data?.lendingDesks
+                .map((desk) =>{
+                  return (
+                  <LoanCardParent desk={desk} status="Defaulted">
+                    <LoanRow loans={desk.loans} status="Defaulted"/>
+                  </LoanCardParent>
+                )
+                })
+            }
           </div>
         </div>
         {/* End Defaulted Row */}
@@ -183,12 +151,72 @@ export const Dashboard = (props: any) => {
           aria-labelledby="pills-completed-tab"
         >
           <div className="row g-4 g-xl-5">
-            <LoanCardParent />
-            <LoanRow loans={[]} />
+            {
+              result?.data?.lendingDesks
+                .map((desk) =>{
+                  return (
+                  <LoanCardParent desk={desk} status="Completed">
+                    <LoanRow loans={desk.loans} status="Completed"/>
+                  </LoanCardParent>
+                )
+                })
+            }
           </div>
         </div>
         {/* End Completed Row */}
       </div>
     </div>
   );
+};
+
+const LoanCardParent = ({desk, children, status}) => {
+  return (
+  <div>
+    <h5>Lending Desk {desk.id}</h5>
+    <div className="col-12">
+      <div className="card border-0 shadow rounded-4 h-100">
+        <div className="card-body">
+          <div className="d-lg-flex">
+            <div className="col-sm-6 col-xl-4">
+              <div className="d-flex align-items-center">
+                <div className="specific-w-50 specific-h-50 d-flex align-items-center justify-content-center bg-primary-subtle text-primary-emphasis rounded-circle flex-shrink-0">
+                  <i className="fa-solid fa-hexagon-vertical-nft h2 m-0"></i>
+                </div>
+                <div className="ps-3">
+                  <h3 className="m-0">{desk.loans.filter((loan) => loan.status === status).length}</h3>
+                  <p className="m-0 text-primary-emphasis">loans</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 col-xl-4">
+              <div className="d-flex align-items-center">
+                <div className="specific-w-50 specific-h-50 d-flex align-items-center justify-content-center bg-primary-subtle text-primary-emphasis rounded-circle flex-shrink-0">
+                  <i className="fa-solid fa-square-dollar h2 m-0"></i>
+                </div>
+                <div className="ps-3">
+                  <h3 className="m-0">{desk.erc20.symbol}</h3>
+                  <p className="m-0 text-primary-emphasis">currency</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 col-xl-4">
+              <div className="d-flex align-items-center">
+                <div className="specific-w-50 specific-h-50 d-flex align-items-center justify-content-center bg-primary-subtle text-primary-emphasis rounded-circle flex-shrink-0">
+                  <i className="fa-solid fa-sack-dollar h2 m-0"></i>
+                </div>
+                <div className="ps-3">
+                  <h3 className="m-0">
+                    {desk.balance} <small className="fw-normal">{desk.erc20.symbol}</small>
+                  </h3>
+                  <p className="m-0 text-primary-emphasis">balance</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    {children}
+  </div>
+)
 };
