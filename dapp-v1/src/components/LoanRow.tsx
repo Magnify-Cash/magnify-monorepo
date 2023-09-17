69cc43d878ff721e3910f52e0961f051296d65a6
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-09-17 10:40:38 -0400

    improve LoanRow timing / progress bar

--- dapp-v1/src/components/LoanRow.tsx
+++ dapp-v1/src/components/LoanRow.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useChainId } from "wagmi";
 import { PopupTransaction } from "@/components";
 import {
@@ -39,206 +39,206 @@ export const LoanRow = ({loans, payback, status, liquidate}: ILoanRowProps) => {
 
   // OK
   return loans.map((loan:Loan) => {
-  // Date rendering
-  const {
-    startDate,
-    endDate,
-    remainingTime,
-    elapsedTime,
-    isTimeLeft,
-  } = calculateTimeInfo(loan?.startTime, loan?.duration);
+    // Date/Time info
+    const [timeInfo, setTimeInfo] = useState(calculateTimeInfo(loan?.startTime, loan?.duration));
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        setTimeInfo(calculateTimeInfo(loan?.startTime, loan?.duration));
+      }, 1000);
+      return () => {
+        clearInterval(intervalId);
+      };
+    }, [loan?.startTime, loan?.duration]);
 
-  // Make Loan Payment Hook
-  const [payBackAmount, setPayBackAmount] = useState("0");
-  const chainId = useChainId();
-  const { writeAsync: approveErc20 } = useErc20Approve({
-    address: loan?.lendingDesk?.erc20.id as `0x${string}`,
-    args: [nftyFinanceV1Address[chainId], BigInt(payBackAmount)],
-  });
-  const { config: makeLoanPaymentConfig } =
-    usePrepareNftyFinanceV1MakeLoanPayment({
-    args: [
-      BigInt(loan?.id || 0), // loan ID
-      BigInt(payBackAmount), // amout
-    ],
-  });
-  const { writeAsync: makeLoanPaymentWrite } = useNftyFinanceV1MakeLoanPayment(makeLoanPaymentConfig)
-  async function makeLoanPayment(loanID: string) {
-    console.log("loanID", loanID);
-    console.log("payBackAmount", payBackAmount);
-    await approveErc20?.()
-    await makeLoanPaymentWrite?.()
-  }
+    // Make Loan Payment Hook
+    const [payBackAmount, setPayBackAmount] = useState("0");
+    const chainId = useChainId();
+    const { writeAsync: approveErc20 } = useErc20Approve({
+      address: loan?.lendingDesk?.erc20.id as `0x${string}`,
+      args: [nftyFinanceV1Address[chainId], BigInt(payBackAmount)],
+    });
+    const { config: makeLoanPaymentConfig } =
+      usePrepareNftyFinanceV1MakeLoanPayment({
+      args: [
+        BigInt(loan?.id || 0), // loan ID
+        BigInt(payBackAmount), // amout
+      ],
+    });
+    const { writeAsync: makeLoanPaymentWrite } = useNftyFinanceV1MakeLoanPayment(makeLoanPaymentConfig)
+    async function makeLoanPayment(loanID: string) {
+      console.log("loanID", loanID);
+      console.log("payBackAmount", payBackAmount);
+      await approveErc20?.()
+      await makeLoanPaymentWrite?.()
+    }
 
+    // Liquidate Overdue loan Hook
+    // Make Loan Payment Hook
+    const { config: liquidateConfig } =
+      usePrepareNftyFinanceV1LiquidateDefaultedLoan({
+      args: [
+        BigInt(loan?.id || 0), // loan ID
+      ],
+    });
+    const { writeAsync: liquidateWrite } = useNftyFinanceV1LiquidateDefaultedLoan(liquidateConfig)
+    async function liquidateOverdueLoan() {
+      await liquidateWrite?.()
+    }
 
-  // Liquidate Overdue loan Hook
-  // Make Loan Payment Hook
-  const { config: liquidateConfig } =
-    usePrepareNftyFinanceV1LiquidateDefaultedLoan({
-    args: [
-      BigInt(loan?.id || 0), // loan ID
-    ],
-  });
-  const { writeAsync: liquidateWrite } = useNftyFinanceV1LiquidateDefaultedLoan(liquidateConfig)
-  async function liquidateOverdueLoan() {
-    await liquidateWrite?.()
-  }
+    return (
+    <div className="col-sm-6 col-xl-4" key={loan?.id}>
+      <style>
+      {`
+        .progress {margin-bottom:0;}
+        .start {float:left;}
+        .end {float:right; text-align:right;}
+      `}
+      </style>
 
-  return (
-  <div className="col-sm-6 col-xl-4" key={loan?.id}>
-    <style>
-    {`
-      .progress {margin-bottom:0;}
-      .start {float:left;}
-      .end {float:right; text-align:right;}
-    `}
-    </style>
-
-    <div className="card border-0 shadow rounded-4 h-100">
-    <div className="card-body">
-      <div className="specific-w-100 specific-h-100 mx-auto d-flex align-items-center justify-content-center bg-primary-subtle text-primary-emphasis rounded-circle overflow-hidden">
-      <img
-        className="object-fit-cover"
-        src="/images/placeholder/doodles.png"
-        alt="test"
-        height="100%"
-      />
+      <div className="card border-0 shadow rounded-4 h-100">
+      <div className="card-body">
+        <div className="specific-w-100 specific-h-100 mx-auto d-flex align-items-center justify-content-center bg-primary-subtle text-primary-emphasis rounded-circle overflow-hidden">
+        <img
+          className="object-fit-cover"
+          src="/images/placeholder/doodles.png"
+          alt="test"
+          height="100%"
+        />
+        </div>
+        <div className="text-center mt-3">
+        <h5>{loan?.nftCollection.id} #{loan?.nftId}</h5>
+        <div className="row g-4">
+          <div className="col-6 bg-info">
+          <i className="fa-regular fa-hand-holding-dollar h1 me-1"></i>
+          <h6>{loan?.amount} {loan?.lendingDesk?.erc20.symbol}</h6>
+          <small>borrowed</small>
+          </div>
+          <div className="col-6 bg-success">
+          <i className="fa-regular fa-calendar h1 me-1"></i>
+          <h6>{loan?.amount - loan?.amountPaidBack} {loan?.lendingDesk?.erc20.symbol}</h6>
+          <small>payoff amount</small>
+          </div>
+          <div className="col-12">
+          {
+            timeInfo.isTimeLeft
+            ? <h5 className="text-start">{timeInfo.remainingTime}</h5>
+            : <p className="text-danger text-start">
+            Loan is overdue! <br/> Make payment or face liquidation.
+            </p>
+          }
+          <div className="progress my-2">
+            <div
+            className="progress-bar progress-bar-striped progress-bar-animated"
+            role="progressbar"
+            aria-valuenow={timeInfo.calculateProgress}
+            aria-valuemin={0}
+            aria-valuemax={100}
+            style={{ width: `${timeInfo.calculateProgress}%` }}
+            />
+          </div>
+          <div className="start">
+            <p className="text-start m-0">{formatTimeInfo(timeInfo.startDate)}</p>
+            <small>issued date</small>
+          </div>
+          <div className="end">
+            <p className="text-end m-0">{formatTimeInfo(timeInfo.endDate)}</p>
+            <small>due date</small>
+          </div>
+          </div>
+        </div>
+        {payback ?
+        <PopupTransaction
+          btnClass="btn btn-primary btn-lg mt-4"
+          btnText="Make Loan Payment"
+          modalId="txModal"
+          modalTitle="Make Loan Payment"
+          modalContent={
+          <div>
+            <small>Loan Details</small>
+            <p>{loan?.nftCollection.id} #{loan?.nftId}</p>
+            <div className="row g-4">
+            <div className="col-6 bg-secondary">
+              <h6>{loan?.amount} {loan?.lendingDesk?.erc20.symbol}</h6>
+              <small>original borrow</small>
+            </div>
+            <div className="col-6 bg-secondary">
+              <h6>{loan?.interest} %</h6>
+              <small>interest date</small>
+            </div>
+            <div className="col-6 bg-secondary">
+              <h6>{timeInfo.elapsedTime}</h6>
+              <small>loan duration</small>
+            </div>
+            <div className="col-6 bg-secondary">
+              <h6>[x]{loan?.lendingDesk?.erc20.symbol}</h6>
+              <small>amount due on expiry date</small>
+            </div>
+            <div className="col-12 bg-success">
+              <h6>[x]{loan?.lendingDesk?.erc20.symbol}</h6>
+              <small>current payoff amount</small>
+            </div>
+            </div>
+            <hr />
+            <p className="text-start">Enter Amount</p>
+            <div className="input-group">
+            <input
+              value={payBackAmount}
+              onChange={(e) => setPayBackAmount(e.target.value)}
+              type="number"
+              className="me-2"
+            />
+            <span>{loan?.lendingDesk?.erc20.symbol}</span>
+            </div>
+            <button type="button" className="btn btn-primary" onClick={() => makeLoanPayment(loan?.id)}>
+            Make Loan Payment
+            </button>
+          </div>
+          }
+        />
+        : null}
+        {liquidate ?
+        <PopupTransaction
+          btnClass="btn btn-primary btn-lg mt-4"
+          btnText="Liquidate Overdue Loan"
+          modalId="txModal"
+          modalTitle="Liquidate Overdue Loan"
+          modalContent={
+          <div>
+            <small>Loan Details</small>
+            <p>{loan?.nftCollection.id} #{loan?.nftId}</p>
+            <div className="row g-4">
+            <div className="col-6 bg-secondary">
+              <h6>{loan?.amount} {loan?.lendingDesk?.erc20.symbol}</h6>
+              <small>original borrow</small>
+            </div>
+            <div className="col-6 bg-secondary">
+              <h6>{loan?.interest} %</h6>
+              <small>interest date</small>
+            </div>
+            <div className="col-6 bg-secondary">
+              <h6>{timeInfo.elapsedTime}</h6>
+              <small>loan duration</small>
+            </div>
+            <div className="col-6 bg-secondary">
+              <h6>[x]{loan?.lendingDesk?.erc20.symbol}</h6>
+              <small>amount due on expiry date</small>
+            </div>
+            <div className="col-12 bg-success">
+              <h6>[x]{loan?.lendingDesk?.erc20.symbol}</h6>
+              <small>current payoff amount</small>
+            </div>
+            </div>
+            <hr />
+            <button type="button" className="btn btn-primary" onClick={() => liquidateOverdueLoan()}>
+            Liquidate Overdue Loan
+            </button>
+          </div>
+          }
+        />
+        : null}
+        </div>
       </div>
-      <div className="text-center mt-3">
-      <h5>{loan?.nftCollection.id} #{loan?.nftId}</h5>
-      <div className="row g-4">
-        <div className="col-6 bg-info">
-        <i className="fa-regular fa-hand-holding-dollar h1 me-1"></i>
-        <h6>{loan?.amount} {loan?.lendingDesk?.erc20.symbol}</h6>
-        <small>borrowed</small>
-        </div>
-        <div className="col-6 bg-success">
-        <i className="fa-regular fa-calendar h1 me-1"></i>
-        <h6>{loan?.amount - loan?.amountPaidBack} {loan?.lendingDesk?.erc20.symbol}</h6>
-        <small>payoff amount</small>
-        </div>
-        <div className="col-12">
-        {
-          isTimeLeft
-          ? <h5 className="text-start">{remainingTime}</h5>
-          : <p className="text-danger text-start">
-          Loan is overdue! <br/> Make payment or face liquidation.
-          </p>
-        }
-        <div className="progress my-2">
-          <div
-          className="progress-bar progress-bar-striped progress-bar-animated"
-          role="progressbar"
-          aria-valuenow={75}
-          aria-valuemin={0}
-          aria-valuemax={100}
-          style={{ width: "75%" }}
-          />
-        </div>
-        <div className="start">
-          <p className="text-start m-0">{formatTimeInfo(startDate)}</p>
-          <small>issued date</small>
-        </div>
-        <div className="end">
-          <p className="text-end m-0">{formatTimeInfo(endDate)}</p>
-          <small>due date</small>
-        </div>
-        </div>
-      </div>
-      {payback ?
-      <PopupTransaction
-        btnClass="btn btn-primary btn-lg mt-4"
-        btnText="Make Loan Payment"
-        modalId="txModal"
-        modalTitle="Make Loan Payment"
-        modalContent={
-        <div>
-          <small>Loan Details</small>
-          <p>{loan?.nftCollection.id} #{loan?.nftId}</p>
-          <div className="row g-4">
-          <div className="col-6 bg-secondary">
-            <h6>{loan?.amount} {loan?.lendingDesk?.erc20.symbol}</h6>
-            <small>original borrow</small>
-          </div>
-          <div className="col-6 bg-secondary">
-            <h6>{loan?.interest} %</h6>
-            <small>interest date</small>
-          </div>
-          <div className="col-6 bg-secondary">
-            <h6>{elapsedTime}</h6>
-            <small>loan duration</small>
-          </div>
-          <div className="col-6 bg-secondary">
-            <h6>[x]{loan?.lendingDesk?.erc20.symbol}</h6>
-            <small>amount due on expiry date</small>
-          </div>
-          <div className="col-12 bg-success">
-            <h6>[x]{loan?.lendingDesk?.erc20.symbol}</h6>
-            <small>current payoff amount</small>
-          </div>
-          </div>
-          <hr />
-          <p className="text-start">Enter Amount</p>
-          <div className="input-group">
-          <input
-            value={payBackAmount}
-            onChange={(e) => setPayBackAmount(e.target.value)}
-            type="number"
-            className="me-2"
-          />
-          <span>{loan?.lendingDesk?.erc20.symbol}</span>
-          </div>
-          <button type="button" className="btn btn-primary" onClick={() => makeLoanPayment(loan?.id)}>
-          Make Loan Payment
-          </button>
-        </div>
-        }
-      />
-      : null}
-      {liquidate ?
-      <PopupTransaction
-        btnClass="btn btn-primary btn-lg mt-4"
-        btnText="Liquidate Overdue Loan"
-        modalId="txModal"
-        modalTitle="Liquidate Overdue Loan"
-        modalContent={
-        <div>
-          <small>Loan Details</small>
-          <p>{loan?.nftCollection.id} #{loan?.nftId}</p>
-          <div className="row g-4">
-          <div className="col-6 bg-secondary">
-            <h6>{loan?.amount} {loan?.lendingDesk?.erc20.symbol}</h6>
-            <small>original borrow</small>
-          </div>
-          <div className="col-6 bg-secondary">
-            <h6>{loan?.interest} %</h6>
-            <small>interest date</small>
-          </div>
-          <div className="col-6 bg-secondary">
-            <h6>{elapsedTime}</h6>
-            <small>loan duration</small>
-          </div>
-          <div className="col-6 bg-secondary">
-            <h6>[x]{loan?.lendingDesk?.erc20.symbol}</h6>
-            <small>amount due on expiry date</small>
-          </div>
-          <div className="col-12 bg-success">
-            <h6>[x]{loan?.lendingDesk?.erc20.symbol}</h6>
-            <small>current payoff amount</small>
-          </div>
-          </div>
-          <hr />
-          <button type="button" className="btn btn-primary" onClick={() => liquidateOverdueLoan()}>
-          Liquidate Overdue Loan
-          </button>
-        </div>
-        }
-      />
-      : null}
       </div>
     </div>
-    </div>
-  </div>
-  )
-  });
+  )});
 };
--- dapp-v1/src/utils.ts
+++ dapp-v1/src/utils.ts
@@ -16,20 +16,7 @@ export function calculateTimeInfo(startTime, durationInHours) {
 
   // Check if remaining duration is negative and set it to zero if needed
   if (remainingDurationInMillis < 0) {
-	remainingDurationInMillis = 0;
-  }
-
-  // Calculate remaining days and hours
-  const remainingDays = Math.floor(remainingDurationInMillis / (24 * 3600 * 1000));
-  remainingDurationInMillis %= 24 * 3600 * 1000;
-  const remainingHours = Math.floor(remainingDurationInMillis / (3600 * 1000));
-
-  // Construct the remaining time string
-  let remainingTime;
-  if (remainingDays > 0) {
-	remainingTime = `${remainingDays} days and ${remainingHours} hours`;
-  } else {
-	remainingTime = `${remainingHours} hours`;
+    remainingDurationInMillis = 0;
   }
 
   // Calculate elapsed duration in milliseconds
@@ -37,33 +24,52 @@ export function calculateTimeInfo(startTime, durationInHours) {
 
   // Check if elapsed duration is negative and set it to zero if needed
   if (elapsedDurationInMillis < 0) {
-	elapsedDurationInMillis = 0;
+    elapsedDurationInMillis = 0;
   }
 
-  // Calculate elapsed days
+  // Calculate total duration in milliseconds
+  const totalDurationInMillis = durationInHours * 3600 * 1000;
+
+  // Calculate progress as a percentage
+  const calculateProgress = (elapsedDurationInMillis / totalDurationInMillis) * 100;
+
+  // Calculate remaining time
+  let remainingTime;
+  if (remainingDurationInMillis > 7 * 24 * 3600 * 1000) {
+    const remainingWeeks = Math.floor(remainingDurationInMillis / (7 * 24 * 3600 * 1000));
+    const remainingDays = Math.floor((remainingDurationInMillis % (7 * 24 * 3600 * 1000)) / (24 * 3600 * 1000));
+    remainingTime = `${remainingWeeks} weeks and ${remainingDays} days`;
+  } else if (remainingDurationInMillis > 24 * 3600 * 1000) {
+    const remainingDays = Math.floor(remainingDurationInMillis / (24 * 3600 * 1000));
+    remainingTime = `${remainingDays} days`;
+  } else if (remainingDurationInMillis > 3600 * 1000) {
+    const remainingHours = Math.floor(remainingDurationInMillis / (3600 * 1000));
+    const remainingMinutes = Math.floor((remainingDurationInMillis % (3600 * 1000)) / (60 * 1000));
+    remainingTime = `${remainingHours} hours and ${remainingMinutes} minutes`;
+  } else {
+    const remainingMinutes = Math.floor(remainingDurationInMillis / (60 * 1000));
+    const remainingSeconds = Math.floor((remainingDurationInMillis % (60 * 1000)) / 1000);
+    remainingTime = `${remainingMinutes} minutes and ${remainingSeconds} seconds`;
+  }
+
+  // Calculate elapsed time
   const elapsedDays = Math.floor(elapsedDurationInMillis / (24 * 3600 * 1000));
-
-  // Calculate elapsed hours
   const elapsedHours = Math.floor((elapsedDurationInMillis % (24 * 3600 * 1000)) / (3600 * 1000));
-
-  // Calculate total days and total hours
-  const totalDays = Math.floor(durationInHours / 24);
-  const totalHours = durationInHours % 24;
-
-  // Construct the elapsed time string
-  const elapsedTime = `${elapsedDays}D ${elapsedHours}HR / ${totalDays}D ${totalHours}HR`;
-
+  const elapsedMinutes = Math.floor((elapsedDurationInMillis % (3600 * 1000)) / (60 * 1000));
+  const elapsedSeconds = Math.floor((elapsedDurationInMillis % (60 * 1000)) / 1000);
+  const elapsedTime = `${elapsedDays}D ${elapsedHours}HR ${elapsedMinutes}MIN ${elapsedSeconds}SEC`;
 
   // Check if there is any time left
   const isTimeLeft = remainingDurationInMillis > 0;
 
   // Return the values as an object
   return {
-	startDate,
-	endDate,
-	remainingTime,
-	elapsedTime,
-	isTimeLeft,
+    startDate,
+    endDate,
+    remainingTime,
+    elapsedTime,
+    isTimeLeft,
+    calculateProgress,
   };
 }
 
