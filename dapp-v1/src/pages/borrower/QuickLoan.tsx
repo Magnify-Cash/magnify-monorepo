d74ebdc6e96f7c2155e7138090015b3c3f644390
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2023-09-18 09:44:04 -0400

    add more options for quickloan

--- dapp-v1/src/pages/borrower/QuickLoan.tsx
+++ dapp-v1/src/pages/borrower/QuickLoan.tsx
@@ -237,8 +237,16 @@ export const QuickLoan = (props: any) => {
                 <label className="form-label">Select NFT</label>
                 <select name="nftID" defaultValue="disabled" className="form-select" onChange={(e) => setNftId(e.target.value)}>
                   <option disabled value="disabled"> -- select an option -- </option>
-                  <option value="1">test</option>
-                  <option value="2">test</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
                 </select>
               </div>
               <div className="mt-3">
