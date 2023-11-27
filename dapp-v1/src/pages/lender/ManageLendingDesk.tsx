a46976045792b6dcf995a38208f1356fe30ef6c5
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2023-11-27 18:32:07 +0530

    Update Manage(edit) lending desk without collection parameters

--- dapp-v1/src/pages/lender/ManageLendingDesk.tsx
+++ dapp-v1/src/pages/lender/ManageLendingDesk.tsx
@@ -81,147 +81,189 @@ export const ManageLendingDesk = (props: any) => {
 
   // Return
   return result.data?.lendingDesk ? (
-      <div className="container-md px-3 px-sm-4 px-xl-5">
-        {/* Demo Row */}
-        <p>
-          <i className="fa-solid fa-arrow-left me-1"></i>
-          <NavLink to="/manage-desks">Back to Manage Lending Desks...</NavLink>
-        </p>
-        <div className="card border-0 shadow rounded-4 mt-4 mt-xl-5">
-          <div className="card-body py-4">
-            <div className="row d-lg-flex align-items-center g-4 g-xl-5">
-              <div className="col-12 d-flex justify-content-between">
-                <h3 className="m-0">
-                  Lending Desk {result.data?.lendingDesk?.id}
-                </h3>
-                <div className="form-check form-switch h3">
-                  <input
-                    onClick={(e) => freezeUnfreeze(e)}
-                    className="form-check-input"
-                    type="checkbox"
-                    role="switch"
-                    defaultChecked={
-                      result.data?.lendingDesk?.status === "Active"
-                    }
-                  />
-                </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="d-flex flex-column align-items-left">
-                  <div className="mt-3">
-                    <p className="m-0">Currency Type</p>
-                    <h5 className="m-0">
+    <div className="container-md px-3 px-sm-4 px-xl-5">
+      <div className="text-body-secondary position-relative">
+        <NavLink to="/manage-desks" className="text-reset text-decoration-none">
+          <i className="fa-light fa-angle-left me-1"></i>
+          Manage Lending Desks
+        </NavLink>
+      </div>
+      <div className="row g-4 mt-n2 mb-4">
+        <div className="col-12">
+          <div className="card border-0 shadow rounded-4">
+            <div className="card-body p-4">
+              <h5 className="fw-medium text-primary-emphasis">
+                Lending Desk {result.data?.lendingDesk?.id}
+              </h5>
+              <div className="container-fluid g-0 mt-4">
+                <div className="row g-4">
+                  <div className="col-lg-4">
+                    <h6 className="fw-medium text-body-secondary">
+                      Currency Type
+                    </h6>
+                    <div className="mt-1 fs-4 d-flex align-items-center">
+                      <img
+                        src="/theme/images/image-13.png" //TODO replace with respective currency image
+                        height="30"
+                        className="d-block rounded-circle flex-shrink-0 me-2"
+                        alt="Image"
+                      />
+                      <div className="text-truncate">
+                        {result.data?.lendingDesk?.erc20.symbol}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4">
+                    <h6 className="fw-medium text-body-secondary">
+                      Available Liquidity
+                    </h6>
+                    <div className="mt-1 fs-4 text-body-secondary">
+                      <strong className="text-primary-emphasis">
+                        {result.data?.lendingDesk?.balance}&nbsp;
+                      </strong>
                       {result.data?.lendingDesk?.erc20.symbol}
-                    </h5>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 text-lg-end">
+                    <PopupTransaction
+                      btnClass="btn btn-primary btn-lg py-2 px-5 rounded-pill"
+                      btnText="Add Funds"
+                      modalId="txModal"
+                      modalTitle="Add Funds"
+                      modalContent={
+                        <div className="text-lg-start mb-2 mt-2">
+                          <div className="card-body">
+                            <h5 className="fw-medium text-body-secondary mb-4">
+                              Deposit Liquidity
+                            </h5>
+                            <div className="input-group ">
+                              <input
+                                value={depositAmount}
+                                // @ts-ignore
+                                onChange={(e) =>
+                                  setDepositAmount(e.target.value)
+                                }
+                                type="number"
+                                className="form-control form-control-lg py-2 mb-4 flex-grow-1"
+                              />
+                              <div className="flex-shrink-0 fs-5 d-flex  ms-3">
+                                <div className="text-truncate ">
+                                  {result.data?.lendingDesk?.erc20.symbol}
+                                </div>
+                              </div>
+                            </div>
+                            <button
+                              type="button"
+                              className="btn btn-primary btn-lg rounded-pill d-block w-100 py-3 lh-1"
+                              onClick={() => depositLiquidity()}
+                            >
+                              Deposit Liquidity
+                            </button>
+                          </div>
+                        </div>
+                      }
+                    />
                   </div>
                 </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="d-flex flex-column align-items-left">
-                  <div className="mt-3">
-                    <p className="m-0">Available Liquidity</p>
-                    <h5 className="m-0">
-                      {result.data?.lendingDesk?.balance}&nbsp;
-                      {result.data?.lendingDesk?.erc20.symbol}
-                    </h5>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 d-flex flex-column">
-                <PopupTransaction
-                  btnClass="btn btn-primary btn-lg mt-4"
-                  btnText="Add Funds"
-                  modalId="txModal"
-                  modalTitle="Add Funds"
-                  modalContent={
-                    <div>
-                      <p>Deposit Liquidity</p>
-                      <div className="input-group">
-                        <input
-                          value={depositAmount}
-                          // @ts-ignore
-                          onChange={(e) => setDepositAmount(e.target.value)}
-                          type="number"
-                          className="me-2"
-                        />
-                        <span>{result.data?.lendingDesk?.erc20.symbol}</span>
-                      </div>
-                      <button type="button" className="btn btn-primary" onClick={() => depositLiquidity()}>
-                      Deposit Liquidity
-                      </button>
-                    </div>
-                  }
-                />
-                <PopupTransaction
-                  btnClass="btn btn-primary btn-lg mt-4"
-                  btnText="Withdraw Funds"
-                  modalId="txModal2"
-                  modalTitle="Withdraw Funds"
-                  modalContent={
-                    <div>
-                      <p>Withdraw Liquidity</p>
-                      <div className="input-group">
-                        <input
-                          value={withdrawAmount}
-                          // @ts-ignore
-                          onChange={(e) => setWithdrawAmount(e.target.value)}
-                          type="number"
-                          className="me-2"
-                        />
-                        <span>{result.data?.lendingDesk?.erc20.symbol}</span>
-                      </div>
-                      <button type="button" className="btn btn-primary" onClick={() => withdrawLiquidity()}>
-                      Withdraw Liquidity
-                      </button>
-                    </div>
-                  }
-                />
               </div>
             </div>
           </div>
         </div>
         {/* End row */}
-        <div className="row">
-          <div className="col-lg-6">
-            <div className="card border-0 shadow rounded-4 mt-4 mt-xl-5">
-              <div className="card-body py-4">
-                {result.data?.lendingDesk?.loanConfigs.map((config, index) => {
-                  return (
-                    <div key={index}>
-                      <p>Collection {index + 1}</p>
-                      <div className="d-flex align-items-center">
-                        <img height="20" width="20" />
-                        <p className="m-0 ms-1">{config.id}</p>
+        <div className="col-xxl-6">
+          <div className="card border-0 shadow rounded-4 h-100 overflow-hidden d-block">
+            <div className="card-body p-4 specific-h-75">
+              <h5 className="fw-medium text-primary-emphasis">
+                {/* Total number of collections */}
+                Collections | {result.data?.lendingDesk?.loanConfigs.length}
+              </h5>
+            </div>
+            <div className="card-body p-4 pt-0 specific-h-xxl-450 overflow-y-auto">
+              {result.data?.lendingDesk?.loanConfigs.map((config, index) => {
+                return (
+                  <div key={index} className="pb-2 mb-2 border-bottom">
+                    <div className="d-flex align-items-center">
+                      <div className="text-body-secondary text-truncate">
+                        Collection {index + 1}
                       </div>
-                      <p>
-                        <strong>Offer:</strong> {config.minAmount} -{" "}
+                      <div className="flex-shrink-0 ms-auto">
+                        <span className="text-body-secondary me-2">
+                          <a
+                            href="#"
+                            className="text-reset text-decoration-none"
+                            aria-lable="Edit"
+                          >
+                            <i className="fa-regular fa-edit"></i>
+                          </a>
+                        </span>
+                        <span className="text-danger-emphasis">
+                          <a
+                            href="#"
+                            className="text-reset text-decoration-none"
+                            aria-lable="Delete"
+                          >
+                            <i className="fa-regular fa-trash-can"></i>
+                          </a>
+                        </span>
+                      </div>
+                    </div>
+                    {/* TODO replace hardcoded values */}
+                    <div className="d-flex align-items-center">
+                      <img
+                        src="/theme/images/image-5.png"
+                        height="24"
+                        className="d-block rounded-circle flex-shrink-0 me-2"
+                      />
+                      <div className="text-truncate fw-medium">
+                        {`Pudgy Penguins`}
+                      </div>
+                    </div>
+                    <div className="mt-2 d-flex align-items-center">
+                      <span className="flex-shrink-0 specific-w-25">
+                        <i className="fa-light fa-hand-holding-dollar text-success-emphasis"></i>
+                      </span>
+                      <div className="text-truncate">
+                        <strong>Offer:</strong> {config.minAmount}-
                         {config.maxAmount}{" "}
                         {result.data?.lendingDesk?.erc20.symbol}
-                      </p>
-                      <p>
-                        <strong>Duration:</strong> {config.minDuration} -{" "}
-                        {config.maxDuration} Days
-                      </p>
-                      <p>
-                        <strong>Interest:</strong> {config.minInterest} -{" "}
-                        {config.maxInterest} %
-                      </p>
+                      </div>
                     </div>
-                  );
-                })}
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-6">
-            <div className="card border-0 shadow rounded-4 mt-4 mt-xl-5">
-              <div className="card-body py-4">
-                <p className="text-primary fw-bold">Collection Parameters</p>
-              </div>
+                    <div className="mt-1 d-flex align-items-center">
+                      <span className="flex-shrink-0 specific-w-25">
+                        <i className="fa-light fa-calendar-clock text-info-emphasis"></i>
+                      </span>
+                      <div className="text-truncate">
+                        <strong>Duration:</strong> {config.minDuration}-
+                        {config.maxDuration} Days
+                      </div>
+                    </div>
+                    <div className="mt-1 d-flex align-items-center">
+                      <span className="flex-shrink-0 specific-w-25">
+                        <i className="fa-light fa-badge-percent text-primary-emphasis"></i>
+                      </span>
+                      <div className="text-truncate">
+                        <strong>Interest Rate:</strong> {config.minInterest}-
+                        {config.maxInterest}%
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
-
-        {/* End Container*/}
+        <div className="col-xxl-6">
+          <div className="card border-0 shadow rounded-4 h-100">
+            <div className="card-body p-4">
+              <h5 className="fw-medium text-primary-emphasis">
+                Collection Paramaters
+              </h5>
+            </div>
+          </div>
+        </div>
       </div>
-  ) : null
+
+      {/* End Container*/}
+    </div>
+  ) : null;
 };
