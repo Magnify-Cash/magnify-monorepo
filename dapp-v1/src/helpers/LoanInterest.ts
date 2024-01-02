d0f6930a3cbed958053e69965743f3594fd6ad2c
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2024-01-02 15:58:28 +0530

    Add helper function to calculate loan interest rate

---
+++ dapp-v1/src/helpers/LoanInterest.ts
@@ -0,0 +1,46 @@
+import { toWei } from "./utils";
+
+const calculateLoanInterest = (
+  loanConfig,
+  amountInput,
+  durationInput,
+  decimals?
+) => {
+  const minAmount = BigInt(loanConfig.minAmount);
+  const maxAmount = BigInt(loanConfig.maxAmount);
+  const minDuration = BigInt(loanConfig.minDuration);
+  const maxDuration = BigInt(loanConfig.maxDuration);
+  const minInterest = BigInt(loanConfig.minInterest);
+  const maxInterest = BigInt(loanConfig.maxInterest);
+
+  const amount = amountInput ? toWei(amountInput, decimals) : minAmount;
+  const duration = BigInt(durationInput ? durationInput * 24 : minDuration);
+
+  const amountFactor = (amount - minAmount) / (maxAmount - minAmount);
+  const durationFactor = (duration - minDuration) / (maxDuration - minDuration);
+  const interestRange = maxInterest - minInterest;
+
+  let interest = minInterest;
+
+  if (minInterest === maxInterest) {
+    return Number(interest) / 100;
+  }
+  if (maxAmount === minAmount && maxDuration === minDuration) {
+    return Number(interest) / 100;
+  }
+  if (minDuration === maxDuration) {
+    interest = minInterest + amountFactor * interestRange;
+  } else if (minAmount === maxAmount) {
+    interest = minInterest + durationFactor * interestRange;
+  } else {
+    //Taking average of amountFactor and durationFactor giving both equal weightage
+    //Diving by BigInt(2) after multiplying by interestRange because of integer division
+    //Otherwise (amountFactor + durationFactor)/2 will produce 0
+  }
+  interest =
+    minInterest + ((amountFactor + durationFactor) * interestRange) / BigInt(2);
+
+  return Number(interest) / 100;
+};
+
+export default calculateLoanInterest;
