ee4d54c2e2461da1fc00932c1fa9315a549c489a
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2024-04-24 13:51:32 -0400

    temp fix: hard-coded base sepolia token/nft lists

--- dapp-v1/src/helpers/tokenUrls.tsx
+++ dapp-v1/src/helpers/tokenUrls.tsx
@@ -2,13 +2,16 @@ const nftListUrlsMap = {
   1: [
     "https://raw.githubusercontent.com/NFTYLabs/nft-lists/master/test/schema/bigexample.nftlist.json",
   ],
-  31337: ["http://localhost:5173/tokenlists/nfts.json"],
+  8453: [],
+  84532: ["http://localhost:5173/tokenlists/nftsBaseSepolia.json"],
+  31337: ["http://localhost:5173/tokenlists/nfts.json"]
 };
 
 const tokenListUrlsMap = {
   1: ["https://tokens.coingecko.com/ethereum/all.json"],
   8453: ["https://tokens.coingecko.com/base/all.json"],
-  31337: ["http://localhost:5173/tokenlists/tokens.json"],
+  84532: ["http://localhost:5173/tokenlists/tokensBaseSepolia.json"],
+  31337: ["http://localhost:5173/tokenlists/tokens.json"]
 };
 
 export function getTokenListUrls(
