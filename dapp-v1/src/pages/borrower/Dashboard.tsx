43d97a832ae736401ebf7a948deb985e60ff1ed7
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2024-07-24 20:48:35 +0530

    Update styling of load more button on Dashboard pages

--- dapp-v1/src/pages/borrower/Dashboard.tsx
+++ dapp-v1/src/pages/borrower/Dashboard.tsx
@@ -19,13 +19,17 @@ const renderBorrowerDashboard = ({
       {loading && <LoadingIndicator />}
       {error && <p>Error: {error.message}</p>}
       {hasNextPage && (
-        <button
-          onClick={loadMore}
-          disabled={loading}
-          className="btn btn-primary d-block mx-auto my-3 px-4 py-2 text-uppercase font-weight-bold"
-        >
-          Load More
-        </button>
+      <div className="row g-4 justify-content-center mt-0">
+        <div className="col-sm-6 col-xl-4 mb-2">
+          <button
+            onClick={loadMore}
+            disabled={loading}
+            className="btn btn-primary d-block mx-auto my-3 px-4 py-2 text-uppercase font-weight-bold"
+          >
+            Load More
+          </button>
+        </div>
+      </div>
       )}
     </>
   );
