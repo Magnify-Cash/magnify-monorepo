4c0ac266f49a5cd2491a6d339bb9f1b447a69ac0
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2024-07-24 18:25:28 +0530

    Update placement of loading indicator in ManageLendingDesks

--- dapp-v1/src/pages/lender/ManageLendingDesks.tsx
+++ dapp-v1/src/pages/lender/ManageLendingDesks.tsx
@@ -7,9 +7,6 @@ import { useAccount } from "wagmi";
 import { ManageLendingDesksDocument } from "../../../.graphclient";
 
 const renderLendingDesks = ({ items, loading, error, loadMore, hasNextPage, props }) => {
-  if (loading) return (
-    <LoadingIndicator/>
-  )
 
   if (items.length === 0) return (
     <div className="specific-w-400 mw-100 mx-auto mt-5 pt-3">
@@ -27,88 +24,126 @@ const renderLendingDesks = ({ items, loading, error, loadMore, hasNextPage, prop
   );
 
   return (
-      <div>
-      {items.map((desk) =>
-      <div
-        className="card bg-primary-subtle border-primary-subtle rounded-4 my-4"
-        key={desk.id}
-      >
-        <div className="card-body p-4">
-          <h5 className="fw-medium text-primary-emphasis">Lending Desk {desk.id}</h5>
-          <div className="container-fluid g-0 mt-4">
-            <div className="row g-4">
-              <div className="col-xl-3">
-                <h6 className="fw-medium text-body-secondary">Currency Type</h6>
-                <div className="d-flex align-items-center text-body-secondary">
-                  <div className="text-truncate">{desk.erc20.symbol}</div>
+    <div>
+      {items.map((desk) => (
+        <div
+          className="card bg-primary-subtle border-primary-subtle rounded-4 my-4"
+          key={desk.id}
+        >
+          <div className="card-body p-4">
+            <h5 className="fw-medium text-primary-emphasis">
+              Lending Desk {desk.id}
+            </h5>
+            <div className="container-fluid g-0 mt-4">
+              <div className="row g-4">
+                <div className="col-xl-3">
+                  <h6 className="fw-medium text-body-secondary">
+                    Currency Type
+                  </h6>
+                  <div className="d-flex align-items-center text-body-secondary">
+                    <div className="text-truncate">{desk.erc20.symbol}</div>
+                  </div>
+                  <br />
+                  <h6 className="fw-medium text-body-secondary">Collections</h6>
+                  <p className="m-0">{desk.loanConfigs.length}</p>
+                  <br />
                 </div>
-                <br />
-                <h6 className="fw-medium text-body-secondary">Collections</h6>
-                <p className="m-0">{desk.loanConfigs.length}</p>
-                <br />
-              </div>
-              <div className="col-xl-3">
-                <h6 className="fw-medium text-body-secondary">Total Loans</h6>
-                <div className="text-body-secondary" style={{ height: "24px" }}>
-                  <strong>{desk.loansCount}</strong>
+                <div className="col-xl-3">
+                  <h6 className="fw-medium text-body-secondary">Total Loans</h6>
+                  <div
+                    className="text-body-secondary"
+                    style={{ height: "24px" }}
+                  >
+                    <strong>{desk.loansCount}</strong>
+                  </div>
+                  <br />
+                  <h6 className="fw-medium text-body-secondary">
+                    Defaulted Loans
+                  </h6>
+                  <div
+                    className="text-body-secondary"
+                    style={{ height: "24px" }}
+                  >
+                    <strong>{desk.loansDefaultedCount}</strong>
+                  </div>
+                  <br />
+                  <h6 className="fw-medium text-body-secondary">
+                    Resolved Loans
+                  </h6>
+                  <div
+                    className="text-body-secondary"
+                    style={{ height: "24px" }}
+                  >
+                    <strong>{desk.loansResolvedCount}</strong>
+                  </div>
+                  <hr className="d-xl-none" />
                 </div>
-                <br />
-                <h6 className="fw-medium text-body-secondary">Defaulted Loans</h6>
-                <div className="text-body-secondary" style={{ height: "24px" }}>
-                  <strong>{desk.loansDefaultedCount}</strong>
+                <div className="col-xl-3">
+                  <h6 className="fw-medium text-body-secondary">
+                    Available Liquidity
+                  </h6>
+                  <div className="text-body-secondary">
+                    <strong>
+                      {fromWei(desk.balance, desk?.erc20?.decimals)}
+                    </strong>
+                    {` ${desk?.erc20?.symbol}`}
+                  </div>
+                  <br />
+                  <h6 className="fw-medium text-body-secondary">
+                    Net Liquidity Issued
+                  </h6>
+                  <div
+                    className="text-body-secondary"
+                    style={{ height: "24px" }}
+                  >
+                    <strong>
+                      {fromWei(desk.netLiquidityIssued, desk?.erc20?.decimals)}{" "}
+                    </strong>
+                    {desk.erc20.symbol}
+                  </div>
+                  <br />
+                  <h6 className="fw-medium text-body-secondary">
+                    Net Profit/Revenue
+                  </h6>
+                  <div
+                    className="text-body-secondary"
+                    style={{ height: "24px" }}
+                  >
+                    <strong>
+                      {fromWei(desk.netProfit, desk?.erc20?.decimals)}{" "}
+                    </strong>{" "}
+                    {desk.erc20.symbol}
+                  </div>
+                  <br />
+                  <hr className="d-xl-none" />
                 </div>
-                <br />
-                <h6 className="fw-medium text-body-secondary">Resolved Loans</h6>
-                <div className="text-body-secondary" style={{ height: "24px" }}>
-                  <strong>{desk.loansResolvedCount}</strong>
+                <div className="col-xl-3 d-flex flex-column ps-xl-4 ps-xxl-5">
+                  <NavLink
+                    to={`/manage-desks/${desk.id}`}
+                    className="btn btn-lg py-2 px-3 focus-ring bg-primary-subtle rounded-pill text-primary-emphasis d-flex align-items-center justify-content-between text-start d-block w-100"
+                  >
+                    Edit <i className="fa-regular fa-pencil" />
+                  </NavLink>
                 </div>
-                <hr className="d-xl-none" />
-              </div>
-              <div className="col-xl-3">
-                <h6 className="fw-medium text-body-secondary">Available Liquidity</h6>
-                <div className="text-body-secondary">
-                  <strong>{fromWei(desk.balance, desk?.erc20?.decimals)}</strong>
-                  {` ${desk?.erc20?.symbol}`}
-                </div>
-                <br />
-                <h6 className="fw-medium text-body-secondary">Net Liquidity Issued</h6>
-                <div className="text-body-secondary" style={{ height: "24px" }}>
-                  <strong>
-                    {fromWei(desk.netLiquidityIssued, desk?.erc20?.decimals)}{" "}
-                  </strong>
-                  {desk.erc20.symbol}
-                </div>
-                <br />
-                <h6 className="fw-medium text-body-secondary">Net Profit/Revenue</h6>
-                <div className="text-body-secondary" style={{ height: "24px" }}>
-                  <strong>{fromWei(desk.netProfit, desk?.erc20?.decimals)} </strong>{" "}
-                  {desk.erc20.symbol}
-                </div>
-                <br />
-                <hr className="d-xl-none" />
-              </div>
-              <div className="col-xl-3 d-flex flex-column ps-xl-4 ps-xxl-5">
-                <NavLink
-                  to={`/manage-desks/${desk.id}`}
-                  className="btn btn-lg py-2 px-3 focus-ring bg-primary-subtle rounded-pill text-primary-emphasis d-flex align-items-center justify-content-between text-start d-block w-100"
-                >
-                  Edit <i className="fa-regular fa-pencil" />
-                </NavLink>
               </div>
             </div>
+            <div className="col-lg-4 d-flex align-items-center" />
           </div>
-          <div className="col-lg-4 d-flex align-items-center" />
         </div>
-      </div>
-    )}
-    {error && <p>Error: {error.message}</p>}
-    {hasNextPage && (
-      <button onClick={loadMore} disabled={loading} className="btn btn-primary">
-        Load More
-      </button>
-    )}
+      ))}
+      {loading && <LoadingIndicator />}
+      {error && <p>Error: {error.message}</p>}
+      {hasNextPage && (
+        <button
+          onClick={loadMore}
+          disabled={loading}
+          className="btn btn-primary d-block mx-auto my-3 px-4 py-2 text-uppercase font-weight-bold"
+        >
+          Load More
+        </button>
+      )}
     </div>
-  )
+  );
 }
 
 export const ManageLendingDesks = (props: any) => {
