156bbacb9c01e77a2df32ded54c45f89740cfc77
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2024-08-06 18:18:34 -0400

    wagmi config

--- dapp-v1/wagmi.config.ts
+++ dapp-v1/wagmi.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig, Config } from "@wagmi/cli";
 import { hardhat, react } from "@wagmi/cli/plugins";
 
-const environment = process.env.NODE_ENV || "development";
-
 const config = async (): Promise<Config> => {
   const response = await import("../deployments.json");
   const data = response.default;
@@ -16,7 +14,8 @@ const config = async (): Promise<Config> => {
           MagnifyCashV1: {
             31337: hardhatAddress, // hardhat
             11155111: "0x58b9F441b5c4681e1Ab74ecdE2A01698831BF2c4", // sepolia
-            84532: "0x9d77E621be4aF95A83c4334e7B51e4440dC2Ed0f" // base sepolia
+            84532: "0x9d77E621be4aF95A83c4334e7B51e4440dC2Ed0f", // base sepolia
+            1: "0x00" // eth mainnet
           },
         },
       }),
