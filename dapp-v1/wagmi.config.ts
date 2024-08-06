907c08c9856b9ea8b7bc8c3d5702094122bf071d
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2024-08-06 19:14:58 -0400

    comment mainnet wagmi config

--- dapp-v1/wagmi.config.ts
+++ dapp-v1/wagmi.config.ts
@@ -14,8 +14,8 @@ const config = async (): Promise<Config> => {
           MagnifyCashV1: {
             31337: hardhatAddress, // hardhat
             11155111: "0x58b9F441b5c4681e1Ab74ecdE2A01698831BF2c4", // sepolia
-            84532: "0x9d77E621be4aF95A83c4334e7B51e4440dC2Ed0f", // base sepolia
-            1: "0x00" // eth mainnet
+            84532: "0x9d77E621be4aF95A83c4334e7B51e4440dC2Ed0f" // base sepolia
+            //1: "0x00" // eth mainnet
           },
         },
       }),
